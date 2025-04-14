105febbe9b7f03b028956b070f51d7e97ee37a1d
Takamichi Omori <takamichi.omori@layerx.co.jp>
2025-04-14 23:04:02 +0900

    chore: add .gitignore file to exclude .DS_Store files from version control feat(raycast): add scripts for opening and appending to Obsidian daily notes The .gitignore file is added to prevent .DS_Store files from being tracked, which helps keep the repository clean. New scripts for Raycast are introduced to streamline the process of opening and appending memos to daily notes in Obsidian, enhancing productivity and user experience.

---
+++ raycast/add_thino.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Append Memos
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🗒️
+# @raycast.argument1 { "type": "text", "placeholder": "Take memos" }
+
+current_time=$(date +"%H:%M")
+memo=$(echo "$1" | sed 's/ /%20/g' )
+open --background "obsidian://advanced-uri?vault=vault&daily=true&mode=append&data=-%20$current_time%20$memo" # MyLifeの部分を自身のvault名に書き換えてください。
---
+++ raycast/open-obsidian-daily-note.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Obsidian Daily Note
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author onsd
+
+open "obsidian://advanced-uri?vault=vault&daily=true"
